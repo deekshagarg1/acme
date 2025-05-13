cfec340875d5c39714372091f6d43c3d196bd1d7
deekshagarg <deekshagarg1405@gmail.com>
2025-05-13 19:23:26 +0530

    add

--- frontend/src/assets/component/home_patientlist/Patient.jsx
+++ frontend/src/assets/component/home_patientlist/Patient.jsx
@@ -34,14 +34,24 @@ const auth =localStorage.getItem("token");
         <th>Place</th>
         <th>Date</th>
         <th>Consultant</th>
-          <th>Delete</th>
+       {auth ? <>   <th>Delete</th></>:<></>}
       </tr>
     </thead>
     <tbody>
       {/* <!-- Repeat the same row --> */}
-     {detail.map((item)=><> <tr><td>{item.name}</td><td>{item.age}</td><td>{item.place}</td><td>{item.date}</td><td>{item.department}</td> <td><button onClick={()=>{delete1(item._id)}} style={{border:"none"}}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="darkred" class="bi bi-trash3" viewBox="0 0 16 16">
+     {detail.map((item)=><> <tr><td>{item.name}</td><td>{item.age}</td><td>{item.place}</td><td>{item.date}</td><td>{item.department}</td>
+
+    {auth ? <>  <td><button onClick={()=>{delete1(item._id)}} style={{border:"none"}}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="darkred" class="bi bi-trash3" viewBox="0 0 16 16">
   <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-</svg></button></td></tr></>)}
+</svg></button></td></> : <></>}
+
+</tr></>)}
+
+
+{auth? <></>: <><tr><td>Ruchika</td><td>30</td><td>Anand nagar</td><td>25-10-2024</td><td>Cardiologist</td></tr>
+<tr><td>Abhinav jain</td><td>25</td><td>vinay nagar</td><td>15-01-2025</td><td>Heptology</td></tr>
+<tr><td>Sneha Dubey</td><td>20</td><td>Tulsi vihar colony</td><td>30-11-2024</td><td>Dermatology</td></tr>
+<tr><td>Bhuvi Goyal</td><td>71</td><td>char sher k naka</td><td>09-03-2025</td><td>	ophthalmology</td></tr></>}
    
     </tbody>
   </table>
